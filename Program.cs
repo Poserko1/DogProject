2a978a8f7f266a8fb9b380ae8eac2d53b4371372
Poserko <kokolin2204@gmail.com>
2023-02-06 09:17:27 +0200

    u

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prazna_Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gospojo");
+        }
+    }
+}
